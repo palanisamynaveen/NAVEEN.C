9da2f9ead337bdc2cd4d7b517339446e7e91698b
palanisamynaveen <35765119+palanisamynaveen@users.noreply.github.com>
2018-02-26 15:59:08 +0530

    26naveen2.c

---
+++ 26naveen2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<conio.h>
+int main(void) 
+{
+	char str[50];
+	int i,count=0;
+  clrscr();
+	printf("enter the sentence:");
+	scanf("%[^\n]s",str);
+	for(i=0;str[i]!='\0';i++)
+	{
+		if(str[i]>='0' && str[i]<='9')
+		{
+			count=count+1;
+		}
+	}
+	printf("\nthe number of numeric in the sentence is:%d",count);
+  getch();
+	return 0;
+}
